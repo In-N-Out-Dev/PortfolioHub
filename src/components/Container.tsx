9f999d2868430ffa3e257be08855c54346910544
Junghoon-P <77766769+Junghoon-P@users.noreply.github.com>
2023-03-27 00:25:45 +0900

    style: remove unused style

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,16 +1,11 @@
-import styled from '@emotion/styled';
 import Introduce from 'components/introduce/Introduce';
 
 const Container = () => {
   return (
-    <Wrap>
+    <>
       <Introduce />
-    </Wrap>
+    </>
   );
 };
-
-const Wrap = styled.div`
-  /* background-color: ${({ theme }) => theme.colors.HIGHTLIGHT}; */
-`;
 
 export default Container;
