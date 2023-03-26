bf30bc54f2c3678a7257266ce6cbd783d68d8470
Junghoon-P <77766769+Junghoon-P@users.noreply.github.com>
2023-03-26 20:56:56 +0900

    feat: add introduce component and mock data

---
+++ src/components/introduce/introduceData.ts
@@ -0,0 +1,13 @@
+const INTRODUCE_DATA = {
+  title: '안녕하세요',
+  contnets:
+    '전공으로 컴퓨터 공학을 공부하고 현재는 웹 개발 분야에서 경력을 쌓고 있습니다.전공으로 컴퓨터 공학을 공부하고 현재는 웹 개발 분야에서 경력을 쌓고 있습니다. 전공으로 컴퓨터 공학을 공부하고 현재는 웹 개발 분야에서 경력을 쌓고 있습니다.',
+  contact: 'whoareyou.gmail.com',
+  resume: [
+    { title: 'github', url: 'https://github.com/' },
+    { title: 'LinkedIn', url: 'https://github.com/' },
+    { title: 'Instagram', url: 'https://github.com/' },
+  ],
+};
+
+export default INTRODUCE_DATA;
