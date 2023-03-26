dca0cee824825fe9ec4876df2d946c3ad66cf334
Hojin An <devian3143@gmail.com>
2023-03-27 02:59:17 +0900

    feat: add SkillsCategory component

---
+++ src/components/skills/SkillsCategory.tsx
@@ -0,0 +1,37 @@
+import styled from '@emotion/styled';
+
+import SkillsCategoryItem from './SkillsCategoryItem';
+
+import type { SkillData } from './SkillsData';
+
+const SkillsCategory = ({ category, skills }: SkillData) => {
+  return (
+    <CategoryCard>
+      <Title>{category}</Title>
+      <SkillsList>
+        {skills.map((skill, i) => (
+          <SkillsCategoryItem
+            {...skill}
+            key={i}
+          />
+        ))}
+      </SkillsList>
+    </CategoryCard>
+  );
+};
+
+const CategoryCard = styled.article``;
+
+const Title = styled.h4`
+  font-family: 'Bebas Neue', cursive;
+  margin-bottom: 16px;
+  font-size: 32px;
+`;
+
+const SkillsList = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  max-width: 480px;
+`;
+
+export default SkillsCategory;
