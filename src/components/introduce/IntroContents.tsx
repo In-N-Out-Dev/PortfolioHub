fcab8df23fc749bde2bc9cac7a0153bfba1b5a18
Junghoon-P <77766769+Junghoon-P@users.noreply.github.com>
2023-03-26 20:56:56 +0900

    feat: add introduce component and mock data

---
+++ src/components/introduce/IntroContents.tsx
@@ -0,0 +1,58 @@
+import styled from '@emotion/styled';
+import Mail from 'assets/Mail';
+import StackButton from 'components/common/StackButton';
+
+import INTRODUCE_DATA from './introduceData';
+
+const IntroContents = () => {
+  return (
+    <Wrap>
+      <Title>{INTRODUCE_DATA.title}</Title>
+      <Contents>{INTRODUCE_DATA.contnets}</Contents>
+      <Contact>
+        <li>
+          <Mail />
+        </li>
+        <li>{INTRODUCE_DATA.contact}</li>
+      </Contact>
+      <Resume>
+        {INTRODUCE_DATA.resume.map((resume, index) => {
+          return <StackButton url={resume.url}>{resume.title}</StackButton>;
+        })}
+      </Resume>
+    </Wrap>
+  );
+};
+
+const Wrap = styled.div`
+  width: 600px;
+  color: ${({ theme }) => theme.colors.MAIN_FONT};
+`;
+
+const Title = styled.h3`
+  margin-bottom: 1rem;
+  font-size: 1.5rem;
+  font-weight: bold;
+`;
+
+const Contents = styled.p`
+  margin-bottom: 1.4rem;
+  font-size: 1rem;
+  line-height: 1.4rem;
+  white-space: normal;
+`;
+
+const Contact = styled.ul`
+  display: flex;
+  align-items: center;
+  gap: 9px;
+  margin-bottom: 1.4rem;
+  font-size: 1.1rem;
+`;
+
+const Resume = styled.ul`
+  display: flex;
+  gap: 14px;
+`;
+
+export default IntroContents;
