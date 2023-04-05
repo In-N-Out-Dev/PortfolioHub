1ecb4a5a7de0d3c2f5c8acd04ff3b20cbacf8c06
Hojin An <devian3143@gmail.com>
2023-04-06 02:24:58 +0900

    feat: impl Contact

---
+++ src/components/contact/Contact.tsx
@@ -0,0 +1,26 @@
+import styled from '@emotion/styled';
+
+import ContactContents from './ContactContents';
+import ContactTitle from './ContactTitle';
+
+import Divider from '@/components/common/Divider';
+
+const Contact = () => {
+  return (
+    <>
+      <Divider />
+      <Section>
+        <ContactTitle />
+        <ContactContents />
+      </Section>
+    </>
+  );
+};
+
+const Section = styled.section`
+  display: flex;
+  justify-content: space-between;
+  height: 9.3rem;
+`;
+
+export default Contact;
