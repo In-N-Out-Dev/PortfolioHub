022e7d616ab14771c1085212f3b2684cd1b035fc
Somi Hwang <somi.c.hwang@gmail.com>
2023-04-05 23:09:50 +0900

    feature: add Blog.tsx file and add it to Home.tsx

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,14 @@
-import Layout from '@/components/common/Layout';
-import Introduce from '@/components/introduce/Introduce';
+import Blog from 'components/blog/Blog';
+import Layout from 'components/common/Layout';
+import Introduce from 'components/introduce/Introduce';
+import Skills from 'components/skills/Skills';
 
 const Home = () => {
   return (
     <Layout>
       <Introduce />
+      <Skills />
+      <Blog />
     </Layout>
   );
 };
