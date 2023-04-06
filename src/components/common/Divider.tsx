6bbf4aefc9f2d7ee25a84e85a749f415e9f92a2a
Junghoon-P <77766769+Junghoon-P@users.noreply.github.com>
2023-04-07 00:32:18 +0900

    refactor: add divider color option

--- src/components/common/Divider.tsx
+++ src/components/common/Divider.tsx
@@ -3,22 +3,25 @@ import styled from '@emotion/styled';
 interface DividerProps {
   height?: number;
   margin?: number;
+  backgroundBlack?: boolean;
 }
 
-const Divider = ({ height = 2, margin = 60 }: DividerProps) => {
+const Divider = ({ height = 2, margin = 60, backgroundBlack = false }: DividerProps) => {
   return (
     <Stroke
       height={height}
       margin={margin}
+      backgroundBlack={backgroundBlack}
     />
   );
 };
 
-const Stroke = styled.div<{ height: number; margin: number }>`
+const Stroke = styled.div<{ height: number; margin: number; backgroundBlack: boolean }>`
   width: 100%;
   height: ${(props) => props.height + 'px'};
   margin-bottom: ${(props) => props.margin + 'px'};
-  background-color: ${({ theme }) => theme.colors.BORDER};
+  background-color: ${({ backgroundBlack, theme }) =>
+    backgroundBlack ? '#000' : theme.colors.BORDER};
 `;
 
 export default Divider;
