29d7981631c0746fd8b9eb841ee1440687a952cc
Hojin An <devian3143@gmail.com>
2023-04-13 14:32:35 +0900

    feat: add portfolio mock data

---
+++ src/components/portfolio/PortfoiloData.ts
@@ -0,0 +1,53 @@
+const portfolioData: PortfolioData[] = [
+  {
+    no: 1,
+    start: '2022.02',
+    end: '2022.05',
+    title: '프로젝트 1',
+    skills: ['React', 'Redux', 'Node.js', 'MongoDB'],
+  },
+  {
+    no: 2,
+    start: '2022.02',
+    end: '2022.05',
+    title: '프로젝트 2',
+    skills: ['Next.js', 'Recoil', 'Node.js', 'MongoDB'],
+  },
+  {
+    no: 3,
+    start: '2022.02',
+    end: '2022.05',
+    title: '프로젝트 3',
+    skills: ['React', 'Redux', 'Node.js', 'MongoDB'],
+  },
+  {
+    no: 4,
+    start: '2022.02',
+    end: '2022.05',
+    title: '프로젝트 4',
+    skills: ['React', 'Redux', 'Node.js', 'MySQL'],
+  },
+  {
+    no: 5,
+    start: '2022.02',
+    end: '2022.05',
+    title: '프로젝트 5',
+    skills: ['React', 'zustand', 'Node.js', 'MongoDB'],
+  },
+  {
+    no: 6,
+    start: '2022.02',
+    end: '2022.05',
+    title: '프로젝트 6',
+    skills: ['React', 'Redux', 'Node.js', 'MongoDB'],
+  },
+  {
+    no: 7,
+    start: '2022.08',
+    end: '2022.11',
+    title: '프로젝트 7',
+    skills: ['React', 'React-Query', 'Node.js', 'MongoDB'],
+  },
+];
+
+export default portfolioData;
