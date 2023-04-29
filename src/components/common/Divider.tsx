4a0f67e9a5462547f9ee779adeb023c7cf2a92a7
Hojin An <devian3143@gmail.com>
2023-04-29 16:43:06 +0900

    refactor: edit Divider component

--- src/components/common/Divider.tsx
+++ src/components/common/Divider.tsx
@@ -2,26 +2,30 @@ import styled from '@emotion/styled';
 
 interface DividerProps {
   height?: number;
-  margin?: number;
-  backgroundBlack?: boolean;
+  marginTop?: number;
+  marginBottom?: number;
 }
 
-const Divider = ({ height = 2, margin = 60, backgroundBlack = false }: DividerProps) => {
+const Divider = ({ height = 0.2, marginTop = 0, marginBottom = 6 }: DividerProps) => {
   return (
     <Stroke
       height={height}
-      margin={margin}
-      backgroundBlack={backgroundBlack}
+      marginTop={marginTop}
+      marginBottom={marginBottom}
     />
   );
 };
 
-const Stroke = styled.div<{ height: number; margin: number; backgroundBlack: boolean }>`
+const Stroke = styled.div<{
+  height: number;
+  marginBottom: number;
+  marginTop: number;
+}>`
   width: 100%;
-  height: ${(props) => props.height + 'px'};
-  margin-bottom: ${(props) => props.margin + 'px'};
-  background-color: ${({ backgroundBlack, theme }) =>
-    backgroundBlack ? '#000' : theme.colors.BORDER};
+  height: ${(props) => props.height + 'rem'};
+  margin-top: ${(props) => props.marginTop + 'rem'};
+  margin-bottom: ${(props) => props.marginBottom + 'rem'};
+  background-color: ${({ theme }) => theme.colors.MAIN_FONT};
 `;
 
 export default Divider;
