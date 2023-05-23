29494053bc5a3c94c3e2a5d75f713ae9ed2d0390
Hojin An <devian3143@gmail.com>
2023-05-24 00:21:11 +0900

    refactor: simplify Skills

--- src/components/skills/SkillsCategory.tsx
+++ src/components/skills/SkillsCategory.tsx
@@ -1,19 +1,26 @@
 import styled from '@emotion/styled';
 
-import SkillsCategoryItemList from './SkillsCategoryItemList';
-
 import type { SkillData } from './SkillsData';
 
 const SkillsCategory = ({ category, skills }: SkillData) => {
   return (
-    <Item>
+    <CategoryCard>
       <Title>{category}</Title>
-      <SkillsCategoryItemList skills={skills} />
-    </Item>
+      <SkillsList>
+        {skills.map(({ highlight, skill }) => (
+          <Item
+            highlight={highlight}
+            key={skill}
+          >
+            {skill}
+          </Item>
+        ))}
+      </SkillsList>
+    </CategoryCard>
   );
 };
 
-const Item = styled.li`
+const CategoryCard = styled.li`
   padding-bottom: 6rem;
   border-bottom: 1px solid ${({ theme }) => theme.colors.MAIN_FONT};
 `;
@@ -23,6 +30,33 @@ const Title = styled.h4`
   margin-bottom: 1.6rem;
   font-size: 3.2rem;
   color: ${({ theme }) => theme.colors.MAIN_FONT};
+`;
+
+const SkillsList = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1.4rem;
+`;
+
+const Item = styled.li<{ highlight: boolean }>`
+  padding: 1rem 1.4rem;
+  width: fit-content;
+  border: 0.1rem solid;
+  border-radius: 5rem;
+  ${({ highlight, theme: { colors } }) => {
+    if (highlight) {
+      return {
+        borderColor: colors.HIGHTLIGHT,
+        backgroundColor: colors.HIGHTLIGHT,
+        color: colors.BACKGROUND,
+      };
+    }
+    return {
+      borderColor: colors.BORDER,
+      backgroundColor: colors.BACKGROUND,
+      color: colors.MAIN_FONT,
+    };
+  }};
 `;
 
 export default SkillsCategory;
