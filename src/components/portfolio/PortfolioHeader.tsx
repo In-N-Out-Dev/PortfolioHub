4a75aac9ee69341b93f9ade9a80ea3cc97ab816c
HojinAn <devian3143@gmail.com>
2023-06-10 15:07:45 +0900

    fix: apply color on Portfolio title

--- src/components/portfolio/PortfolioHeader.tsx
+++ src/components/portfolio/PortfolioHeader.tsx
@@ -25,6 +25,7 @@ const Title = styled.h3`
   font-family: 'Bebas Neue', -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu,
     Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
   font-size: 4.8rem;
+  color: ${({ theme }) => theme.colors.MAIN_FONT};
 `;
 
 export default PortfolioHeader;
