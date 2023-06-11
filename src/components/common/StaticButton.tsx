deca0b50b2d55448daef85a9a08cc0380276cd14
Junghoon-P <77766769+Junghoon-P@users.noreply.github.com>
2023-06-11 18:05:22 +0900

    refactor: change button components to common layout

---
+++ src/components/common/StaticButton.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+
+interface StaticButtonProps {
+  title: string;
+  onClickAction: () => void;
+  children?: React.ReactNode;
+  isColorInversion?: boolean;
+}
+
+const StaticButton = ({
+  title,
+  onClickAction,
+  children,
+  isColorInversion = false,
+}: StaticButtonProps) => {
+  return (
+    <ButtonWrapper
+      onClick={onClickAction}
+      isColorInversion={isColorInversion}
+    >
+      <p>{title}</p>
+      {children}
+    </ButtonWrapper>
+  );
+};
+
+const ButtonWrapper = styled.button<{ isColorInversion: boolean }>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 1rem 1.4rem;
+  border-radius: 5rem;
+  background: ${({ isColorInversion, theme }) =>
+    isColorInversion ? theme.colors.BACKGROUND : theme.colors.MAIN_FONT};
+  color: ${({ isColorInversion, theme }) =>
+    isColorInversion ? theme.colors.MAIN_FONT : theme.colors.BACKGROUND};
+
+  ${({ isColorInversion, theme }) =>
+    isColorInversion && BorderStyle(isColorInversion, theme.colors.MAIN_FONT)}
+`;
+
+const BorderStyle = (isColorInversion: boolean, color: string) => css`
+  border: 1px solid ${isColorInversion && color};
+`;
+
+export default StaticButton;
