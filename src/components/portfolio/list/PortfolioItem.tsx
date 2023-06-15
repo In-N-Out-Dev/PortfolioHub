a00847bb58a8fe1d9a09ba69acb215d7c74a6ef7
HojinAn <devian3143@gmail.com>
2023-06-15 23:48:21 +0900

    refactor: PortfolioList responsive ui

--- src/components/portfolio/list/PortfolioItem.tsx
+++ src/components/portfolio/list/PortfolioItem.tsx
@@ -1,11 +1,57 @@
+import { useMediaQuery } from 'react-responsive';
+
 import styled from '@emotion/styled';
+import { ModalType, useModalStore } from 'stores/useModalStore';
 import { PortfolioData } from 'types/portfolio';
 
 import PortfolioItemSkills from './PortfolioItemSkills';
 
 const PortfolioItem = ({ no, title, skills, start, end }: PortfolioData) => {
+  const { setIsModalOn, modalState } = useModalStore((state) => ({
+    setIsModalOn: state.setIsModalOn,
+    modalState: state.modalState,
+  }));
+
+  const openModal = () => {
+    setIsModalOn(ModalType.PORTFOLIO_DETAILED, true);
+  };
+
+  const isSmallDisplay = useMediaQuery({ query: '(max-width: 320px)' });
+  const isMediumDisplay = useMediaQuery({ query: '(max-width: 768px)' });
+
+  if (isSmallDisplay) {
+    return (
+      <Item onClick={openModal}>
+        <SmallWrapper>
+          <CardNo>{String(no).padStart(2, '0')}</CardNo>
+          <TitleText>{title}</TitleText>
+          <InfoDate>
+            {start} - {end}
+          </InfoDate>
+        </SmallWrapper>
+      </Item>
+    );
+  }
+
+  if (isMediumDisplay) {
+    return (
+      <Item onClick={openModal}>
+        <MediumWrapper>
+          <CardNo>{String(no).padStart(2, '0')}</CardNo>
+          <MediumTitleWrapper>
+            <TitleText>{title}</TitleText>
+            <PortfolioItemSkills skills={skills} />
+          </MediumTitleWrapper>
+        </MediumWrapper>
+        <InfoDate>
+          {start} - {end}
+        </InfoDate>
+      </Item>
+    );
+  }
+
   return (
-    <Item>
+    <Item onClick={openModal}>
       <TitleWrapper>
         <CardNo>{String(no).padStart(2, '0')}</CardNo>
         <TitleText>{title}</TitleText>
@@ -23,12 +69,16 @@ const PortfolioItem = ({ no, title, skills, start, end }: PortfolioData) => {
 const Item = styled.li`
   display: flex;
   justify-content: space-between;
-  align-items: center;
-  padding: 4rem 0;
   ${({ theme: { colors } }) => ({
     color: colors.MAIN_FONT,
   })};
   border-bottom: 0.1rem solid ${({ theme }) => theme.colors.MAIN_FONT};
+  align-items: center;
+  padding: 4rem 0;
+  ${({ theme }) => theme.breakPoint.medium} {
+    padding: 6rem 0;
+    align-items: flex-start;
+  }
 `;
 
 const TitleWrapper = styled.div`
@@ -39,12 +89,24 @@ const TitleWrapper = styled.div`
 const CardNo = styled.p`
   font-size: 3.2rem;
   font-family: 'Bebas Neue', cursive;
-  font-weight: 700;
+  font-weight: 500;
+  ${({ theme }) => theme.breakPoint.medium} {
+    font-size: 2.4rem;
+  }
+  ${({ theme }) => theme.breakPoint.small} {
+    font-size: 2rem;
+  }
 `;
 
 const TitleText = styled.h4`
   font-size: 3rem;
   font-weight: 700;
+  ${({ theme }) => theme.breakPoint.medium} {
+    font-size: 2.4rem;
+  }
+  ${({ theme }) => theme.breakPoint.small} {
+    font-size: 2rem;
+  }
 `;
 
 const InfoWrapper = styled.div`
@@ -58,6 +120,31 @@ const InfoDate = styled.p`
   font-size: 2rem;
   font-weight: 400;
   color: ${({ theme }) => theme.colors.MAIN_FONT};
+  ${({ theme }) => theme.breakPoint.medium} {
+    font-size: 1.6rem;
+  }
+  ${({ theme }) => theme.breakPoint.small} {
+    font-size: 1.4rem;
+  }
+`;
+
+const SmallWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  align-items: 'flex-start';
+`;
+
+const MediumWrapper = styled.div`
+  display: flex;
+  gap: 2rem;
+  align-items: 'flex-start';
+`;
+
+const MediumTitleWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
 `;
 
 export default PortfolioItem;
