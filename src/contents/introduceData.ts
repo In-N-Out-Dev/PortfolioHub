2624c6510aa608ea4e1d2e6234b01c3813600281
Somi Hwang <somi.c.hwang@gmail.com>
2023-07-13 22:22:32 +0900

    FIX: typo for content

--- src/contents/introduceData.ts
+++ src/contents/introduceData.ts
@@ -1,7 +1,7 @@
 const INTRODUCE_DATA = {
   title: '안녕하세요.',
   name: '홍길동',
-  contnets:
+  contents:
     '전공으로 컴퓨터 공학을 공부하고 현재는 웹 개발 분야에서 경력을 쌓고 있습니다.전공으로 컴퓨터 공학을 공부하고 현재는 웹 개발 분야에서 경력을 쌓고 있습니다. 전공으로 컴퓨터 공학을 공부하고 현재는 웹 개발 분야에서 경력을 쌓고 있습니다.',
   contact: 'whoareyou.gmail.com',
   resume: [
--- src/contents/projectData.ts
+++ src/contents/projectData.ts
@@ -2,7 +2,7 @@ const PROJECT_DATA = [
   {
     company: '회사이름 작성란 입니다..',
     position: 'Front-end',
-    contnets:
+    contents:
       '프로젝트의 진행상황을 모니터링하고 팀원들 간의 협업을 위한 웹 애플리케이션 개발을 맡았습니다. React와 Redux를 사용하여 UI를 구현하고, Node.js와 MongoDB를 이용하여 백엔드를 구축했습니다.',
     date: '2023.00.00-2023.00.00',
     projectFeature: ['구현내용1', '구현내용1', '구현내용1', '구현내용1'],
@@ -12,7 +12,7 @@ const PROJECT_DATA = [
   {
     company: '회사이름 작성란 입니다..',
     position: 'Front-end',
-    contnets:
+    contents:
       '프로젝트의 진행상황을 모니터링하고 팀원들 간의 협업을 위한 웹 애플리케이션 개발을 맡았습니다. React와 Redux를 사용하여 UI를 구현하고, Node.js와 MongoDB를 이용하여 백엔드를 구축했습니다.',
     date: '2023.00.00-2023.00.00',
     projectFeature: ['구현내용1', '구현내용1', '구현내용1', '구현내용1'],
@@ -22,7 +22,7 @@ const PROJECT_DATA = [
   {
     company: '회사이름 작성란 입니다..',
     position: 'Front-end',
-    contnets:
+    contents:
       '프로젝트의 진행상황을 모니터링하고 팀원들 간의 협업을 위한 웹 애플리케이션 개발을 맡았습니다. React와 Redux를 사용하여 UI를 구현하고, Node.js와 MongoDB를 이용하여 백엔드를 구축했습니다.',
     date: '2023.00.00-2023.00.00',
     projectFeature: ['구현내용1', '구현내용1', '구현내용1', '구현내용1'],
