85c255c5b6b5a5c11a558c74f012c42ca8040135
Somi Hwang <somi.c.hwang@gmail.com>
2023-07-15 10:31:40 +0900

    feat: page title interaction
    Co-authored-by: Junghoon-P <Junghoon-P@users.noreply.github.com>
    Co-authored-by: 안호진 <HojinAn@users.noreply.github.com>

--- src/styles/interaction.ts
+++ src/styles/interaction.ts
@@ -9,3 +9,23 @@ export const contentVariants: Variants = {
   onscreen: { y: 0, opacity: 1, transition: { duration: 1.5 } },
   offscreen: { y: 100, opacity: 0 },
 };
+
+export const pageTitleJobPositionTextVariants: Variants = {
+  onscreen: { y: 0, x: 0, opacity: 1, transition: { duration: 0.5 } },
+  offscreen: { y: 200, opacity: 0 },
+};
+
+export const pageTitleNameTextVariants: Variants = {
+  onscreen: { y: 0, opacity: 1, transition: { duration: 0.5, delay: 0.2 } },
+  offscreen: { y: 200, opacity: 0 },
+};
+
+export const pageTitleHorizontalRightVariants: Variants = {
+  onscreen: { x: 500, transition: { duration: 2 } },
+  offscreen: { x: 0, transition: { duration: 2 } },
+};
+
+export const pageTitleHorizontalLeftVariants: Variants = {
+  onscreen: { x: -500, transition: { duration: 2 } },
+  offscreen: { x: 0, transition: { duration: 2 } },
+};
