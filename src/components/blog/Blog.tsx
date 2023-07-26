450111f766c7477d505c950bfc45c5a7b20a0826
Somi Hwang <somi.c.hwang@gmail.com>
2023-07-26 22:03:24 +0900

    FIX: intersection observer issue

--- src/components/blog/Blog.tsx
+++ src/components/blog/Blog.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 
+import styled from '@emotion/styled';
 import BlogContents from 'components/blog/BlogContents';
 import ListCard from 'components/common/ListCard';
 import { SectionProps, ViewModeState } from 'types/props';
@@ -7,7 +8,8 @@ import { SectionProps, ViewModeState } from 'types/props';
 const Blog = ({ forwardRef }: SectionProps) => {
   const [blogViewState, setBlogViewState] = useState<ViewModeState>('GALLERY');
   return (
-    <div ref={forwardRef}>
+    <Wrapper>
+      <IntersectionDummyDiv ref={forwardRef} />
       <ListCard
         title="BLOG"
         ViewModeState={blogViewState}
@@ -15,8 +17,21 @@ const Blog = ({ forwardRef }: SectionProps) => {
       >
         <BlogContents blogViewState={blogViewState} />
       </ListCard>
-    </div>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  position: relative;
+`;
+
+const IntersectionDummyDiv = styled.div`
+  position: absolute;
+  top: 0;
+  height: 50vh;
+  width: 100%;
+  background-color: transparent;
+  z-index: -1;
+`;
 
 export default Blog;
